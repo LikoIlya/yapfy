1e4d17b7786f41869e9f127054ede5bf970528dd
Illia Likhoshva <ilyaliko64@gmail.com>
2022-04-15 10:23:25 +0300

    Fix migrations

--- scripts/commands/migrate/index.ts
+++ scripts/commands/migrate/index.ts
@@ -17,14 +17,14 @@ export const addMigrateCommand = (program: Command) => {
     .requiredOption<number>(
       "-s, --from <from>",
       "the migrations counter to start with",
-      (val, prev = 0) => parseInt(val) + prev,
+      (val, def = 0) => !isNaN(parseInt(val)) ? parseInt(val) : def,
       0
     )
     .requiredOption<number>(
       "-e, --to <to>",
       "the migrations counter to end with",
-      (val, prev = 0) => parseInt(val) + prev,
-      migrations.length - 1
+      (val, def = 0) => !isNaN(parseInt(val)) ? parseInt(val) : def,
+      migrations.length > 0 ? migrations.length - 1 : 0
     )
     .requiredOption(
       "-k --key <key>",
