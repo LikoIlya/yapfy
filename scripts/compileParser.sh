48b4ad4781e1044dac40c11d242922a03a34326a
Illia Likhoshva <ilyaliko64@gmail.com>
2022-04-20 11:26:32 +0300

    Add sed script to replace UBINETIC oracle variable inside constants before deploy

--- scripts/compileParser.sh
+++ scripts/compileParser.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
+if [ "$(echo "${1}" | tr '[:upper:]' '[:lower:]')" = "ctez" ]; then
+  set -o allexport
+  [[ -f .env ]] && source .env
+  UBINETIC_ORACLE="${UBINETIC_ORACLE:-tz1ZZZZZZZZZZZZZZZZZZZZZZZZZZZZNkiRg}"
+  sed -i.bak "s/{env.UBINETIC_ORACLE}/\"$UBINETIC_ORACLE\"/gi" ./contracts/partial/parser/vendor/ctez/constants.ligo
+fi
 yarn cli compile -F tz -c parser/${1}
+if [ "$(echo "${1}" | tr '[:upper:]' '[:lower:]')" = "ctez" ]; then
+  mv ./contracts/partial/parser/vendor/ctez/constants.ligo.bak ./contracts/partial/parser/vendor/ctez/constants.ligo
+  set +o allexport
+fi
 
