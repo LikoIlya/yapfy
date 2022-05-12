7aa26c59fdebf163c56b7dd4dc5bb0f88304dbe7
Illia Likhoshva <ilyaliko64@gmail.com>
2022-05-12 10:47:18 +0300

    add silet executing

--- tests/utils/prepare_ctez.ts
+++ tests/utils/prepare_ctez.ts
@@ -1,9 +1,9 @@
 import shell from "shelljs";
 import { TezosAddress } from "../../utils/helpers";
 
-export function prepareCtezBytes(ubiOracle: TezosAddress) {
+export function prepareCtezBytes(ubiOracle: TezosAddress, silent = false): string {
   try {
-    return shell.exec(`UBINETIC_ORACLE=${ubiOracle} ./scripts/parserToBytes.sh ctez`);
+    return shell.exec(`UBINETIC_ORACLE=${ubiOracle} ./scripts/parserToBytes.sh ctez`, { silent });
   }
   catch (e) {
     console.error(e);
