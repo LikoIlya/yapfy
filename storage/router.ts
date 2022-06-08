5742728d6184ee3fccc9953fd5c5cd2c6ed28664
Illia Likhoshva <ilyaliko64@gmail.com>
2022-06-08 11:16:44 +0300

    Upd meta: version, pushed to IPFS

--- storage/router.ts
+++ storage/router.ts
@@ -3,18 +3,22 @@ import { BytesString, TezosAddress } from '../utils/helpers';
 import BigNumber from "bignumber.js";
 
 const metadata = MichelsonMap.fromLiteral({
-  "": Buffer.from("tezos-storage:router", "ascii").toString("hex"),
-  router: Buffer.from(
-    JSON.stringify({
-      name: "Yupana PF Router",
-      version: "v1.0.0",
-      description: "Yupana protocol Price Feed main router.",
-      authors: ["Madfish.Solutions <https://www.madfish.solutions>"],
-      homepage: "https://yupana.com",
-      interfaces: ["TZIP-16"],
-    }),
-    "ascii"
-  ).toString("hex"),
+  "": Buffer.from("ipfs://QmZTCZ4phYTLfCGs2cvCsVvcSEqpvGFbtze3qAVzbW4M73", "ascii").toString("hex"),
+  // router: Buffer.from(
+  //   JSON.stringify({
+  //     name: "Yupana PF Router",
+  //     version: "v0.1.1-tez",
+  //     description: "Yupana protocol Price Feed main router.",
+  //     authors: ["Madfish.Solutions <https://www.madfish.solutions>"],
+  //     "source": {
+  //       "tools": ["Ligo", "Flextesa"],
+  //       "location": "https://github.com/madfish-solutions/yapfy/blob/v0.1.1-tez/contracts/main/router.ligo"
+  //     },
+  //     homepage: "https://yupana.finance",
+  //     interfaces: ["TZIP-016"],
+  //   }),
+  //   "ascii"
+  // ).toString("hex"),
 });
 
 export default {
