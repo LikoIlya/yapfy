43a954bdcdbe2873e4e5dfa4442b373313f283e1
Illia Likhoshva <ilyaliko64@gmail.com>
2022-07-04 21:53:36 +0300

    Update metadata version

--- contracts/partial/router/constants.ligo
+++ contracts/partial/router/constants.ligo
@@ -1,15 +1,15 @@
 const default_parser_metadata: big_map(string, bytes) = big_map[
-  "" -> 0x697066733a2f2f516d62365a6e736938554637727550386651354a756661625568735442746b44796f444b465636476f4c56697172;
+  "" -> 0x22697066733a2f2f516d5375554d395933617179574e43386f6b69674a5a3738734a4873576e71426e5138623655533546787171327622;
 ];
 // {
 //   "name": "Yupana PF Parser",
-//   "version": "v0.1.1-tez",
+//   "version": "v0.1.2",
 //   "description": "Price parser for Yupana Price Feed proxy",
 //   "homepage": "https://yupana.finance",
 //   "interfaces": ["TZIP-016"],
 //   "authors": ["Madfish.Solutions <https://www.madfish.solutions>"],
 //   "source": {
 //     "tools": ["Ligo", "Flextesa"],
-//     "location": "https://github.com/madfish-solutions/yapfy/blob/v0.1.1-tez/contracts/main/parser"
+//     "location": "https://github.com/madfish-solutions/yapfy/blob/v0.1.2/contracts/main/parser"
 //   }
 // }
--- storage/router.ts
+++ storage/router.ts
@@ -3,16 +3,16 @@ import { BytesString, TezosAddress } from '../utils/helpers';
 import BigNumber from "bignumber.js";
 
 const metadata = MichelsonMap.fromLiteral({
-  "": Buffer.from("ipfs://QmZTCZ4phYTLfCGs2cvCsVvcSEqpvGFbtze3qAVzbW4M73", "ascii").toString("hex"),
+  "": Buffer.from("ipfs://QmSuUM9Y3aqyWNC8okigJZ78sJHsWnqBnQ8b6US5Fxqq2v", "ascii").toString("hex"),
   // router: Buffer.from(
   //   JSON.stringify({
   //     name: "Yupana PF Router",
-  //     version: "v0.1.1-tez",
+  //     version: "v0.1.2",
   //     description: "Yupana protocol Price Feed main router.",
   //     authors: ["Madfish.Solutions <https://www.madfish.solutions>"],
   //     "source": {
   //       "tools": ["Ligo", "Flextesa"],
-  //       "location": "https://github.com/madfish-solutions/yapfy/blob/v0.1.1-tez/contracts/main/router.ligo"
+  //       "location": "https://github.com/madfish-solutions/yapfy/blob/v0.1.2/contracts/main/router.ligo"
   //     },
   //     homepage: "https://yupana.finance",
   //     interfaces: ["TZIP-016"],
